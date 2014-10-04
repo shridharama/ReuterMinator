d5268861ff97863b24092e0de598aca075d0472b
shridhar <rama.shridhar@gmail.com>
2014-10-03 21:35:31 -0400

    adding preprocessor helper

---
+++ reuterminator/preprocessorhelper.py
@@ -0,0 +1,40 @@
+import nltk
+import string
+import json
+from nltk import *
+from nltk import corpus
+from nltk.stem import PorterStemmer
+from nltk.tokenize import RegexpTokenizer
+import sys
+import os
+import os.path
+import json
+import time
+
+
+JUNK_WORDS = ['<','>',':',"''",'#','cc','',',','s','reuter','note']
+
+
+''' Class that contains auxiliary (static) helper methods that are used by Preprocessor '''
+class PreprocessorHelper:
+
+    @staticmethod
+    def write_to_file(data, filename):
+        #Converts to json and dumps the contents to a file
+        with open(filename, 'w') as outfile:
+            #Removing non-unicode characters from the dataset
+            #self.parsed_data = unicode(self.parsed_data, errors='ignore')
+            json.dump(data, outfile, indent=4)
+        outfile.close()
+
+    @staticmethod
+    def get_words_after_stop_word_removal(tokens):
+        good_words = [w for w in tokens if w.lower() not in nltk.corpus.stopwords.words('english')]
+        better_words = [w for w in good_words if w.lower() not in JUNK_WORDS]
+        return better_words
+
+    @staticmethod
+    def get_stemmed_words(tokens):
+        stemmer = PorterStemmer()
+        stemmed_words = [stemmer.stem(w) for w in tokens]
+        return stemmed_words
