db3cfa6b120eaf44d71018faa2b462348704e6be
kaushlakers <kaush.lakers@gmail.com>
2014-10-08 19:04:01 -0400

    Few more changes to preprocessor and classifier. Also included word dict py file

--- reuterminator/classifier.py
+++ reuterminator/classifier.py
@@ -4,6 +4,9 @@ from sklearn.naive_bayes import *
 from sklearn.svm import SVC
 from sklearn.tree import *
 import time
+from sklearn.feature_extraction import DictVectorizer
+from nltk.classify import DecisionTreeClassifier
+import numpy as np
 class Classifier:
     def __init__(self, feature_type):
         self.feature_type = feature_type
@@ -22,9 +25,9 @@ class Classifier:
                 i = i+1
                 for word,freq in feature_vector['term_frequencies'].iteritems():
                     if word not in word_dict:
-                        word_dict[word] = 0 #freq
-                    #else:
-                        #word_dict[word] = freq
+                        word_dict[word] = 1 #freq
+                    else:
+                        word_dict[word] += 1
 
         print i
         s=open('word_dict.json','w')
@@ -37,16 +40,41 @@ class Classifier:
         self.preprare_train_and_test_set()
 
 
-    def classify(self, classifier_type):
-        classifier = SklearnClassifier(classifier_type)
+    def classify_naive_bayes(self, classifier_type):
+        #classifier = SklearnClassifier(classifier_type)
+        clf = classifier_type
+        X = np.array([feature_vector[0] for feature_vector in self.feature_vectors_tuples_for_train])
+        Y = np.array([feature_vector[1] for feature_vector in self.feature_vectors_tuples_for_train])
         print "training"
-        classifier.train(self.feature_vectors_tuples_for_train)
+        vectorizer = DictVectorizer(dtype=float, sparse=True)
+        X = vectorizer.fit_transform(X)
+        clf.fit(X,Y)
+        #classifier.train(self.feature_vectors_tuples_for_train)
+        print "testing classifier"
+        X_test = vectorizer.transform(np.array([feature_vector[0] for feature_vector in self.feature_vectors_tuples_for_test]))
+        classified_labels = clf.predict(X_test)
+        #for i in range(0,len(predicted_list)):
+         #   print predicted_list[i] == self.feature_vectors_tuples_for_test[i][1]
+        #classified_labels =  classifier.batch_classify([feature_set_tuple[0] for feature_set_tuple in self.feature_vectors_tuples_for_test])
+        correct = 0
+        wrong = 0
+        for i in range(0, len(classified_labels)):
+            #print classified_labels[i] +' ' + self.feature_vectors_tuples_for_test[i][1]
+            if classified_labels[i] == self.feature_vectors_tuples_for_test[i][1]:
+                correct += 1
+            else:
+                wrong += 1
+        print correct, wrong
+
+    def classify_decision_tree(self):
+
+        print "training decision tree"
+        classifier = DecisionTreeClassifier.train(self.feature_vectors_tuples_for_train, depth_cutoff=200, entropy_cutoff=0.1)
         print "testing classifier"
         classified_labels =  classifier.batch_classify([feature_set_tuple[0] for feature_set_tuple in self.feature_vectors_tuples_for_test])
         correct = 0
         wrong = 0
         for i in range(0, len(classified_labels)):
-            #print classified_labels[i] +' ' + self.feature_vectors_tuples_for_test[i][1]
             if classified_labels[i] is self.feature_vectors_tuples_for_test[i][1]:
                 correct += 1
             else:
@@ -54,6 +82,7 @@ class Classifier:
         print correct, wrong
 
 
+    #separates train and test data
     def preprare_train_and_test_set(self):
         for topic,topic_vector in self.feature_vectors.iteritems():
             train_data_limit = int(0.8*len(topic_vector))
@@ -84,11 +113,16 @@ class Classifier:
 
 def main():
     classifier = Classifier('term_frequencies')
-    start = time.clock()
     classifier.prepare_data_for_classify()
-    classifier.classify(MultinomialNB())
+    start = time.clock()
+    classifier.classify_naive_bayes(MultinomialNB())
     end = time.clock()
-    print end - start, 'seconds to train and classify'
+    print end - start, 'seconds to train and classify with naive_bayes'
 
+    #code for decision tree. Not sure it works well yet
+    #start = time.clock()
+    #classifier.classify_decision_tree()
+    #end = time.clock()
+    #print end - start, 'seconds to train and classify with decision tree'
     #classifier.create_word_dict()
 if __name__ == "__main__": main()
--- reuterminator/preprocessor.py
+++ reuterminator/preprocessor.py
@@ -57,6 +57,7 @@ class Preprocessor:
         self.global_bigram_dict = {}
         #self.data_set_directory = d
 
+
     def get_parsed_data(self):
         print 'Retrieving parsed data from datasets'
         self.parser.parse_all_docs()
@@ -71,7 +72,7 @@ class Preprocessor:
     def clear_topic_dict(self):
         for topic,docs in self.topic_wise_dict.iteritems():
             self.topic_wise_dict[topic].clear()
-        print self.topic_wise_dict
+
     def clean_data(self):
         i=0
         for doc_id,doc_attributes in self.parsed_data.iteritems():
@@ -93,12 +94,13 @@ class Preprocessor:
 
 
     def populate_tfidf_feature_vector_dictionary(self):
-        self.calculate_term_and_document_frequencies()
-        self.calculate_tfidf()
-        self.populate_dictionary_with_class_labels(self.tfidf_dict)
+        self.calculate_term_frequencies()
+        #self.calculate_term_and_document_frequencies()
+        #self.calculate_tfidf()
+        #self.populate_dictionary_with_class_labels(self.tfidf_dict)
 
     def populate_term_frequencies_feature_vector_dictionary(self):
-        self.remove_low_frequency_words()
+        #self.remove_low_frequency_words()
         #tf_dict now contains list of term frequencies for each document
         self.populate_dictionary_with_class_labels(self.tf_dict)
 
@@ -121,6 +123,23 @@ class Preprocessor:
             self.bigram_dict[doc_id]['bigrams_pmi'] = {}
             self.bigram_dict[doc_id]['bigrams_pmi'] = self.get_best_bigrams_from_doc(doc_id)
         self.populate_dictionary_with_class_labels(self.bigram_dict)
+
+
+
+    #wrote separate function to calculate term frequency data matrix
+    def calculate_term_frequencies(self):
+        vector_template = self.convert_to_utf(json.load(open('word_dict.json')))
+        for doc_id,tokenized_body_cleaned_list in self.tokenized_body_cleaned_dict.iteritems():
+            tf_data_matrix = {}
+            tf_data_matrix = dict.fromkeys(vector_template,0)
+            frequency_dist = FreqDist(tokenized_body_cleaned_list)
+            self.tf_dict[doc_id] = {'term_frequencies':{}}
+
+            for token in frequency_dist:
+                self.tf_dict[doc_id]['term_frequencies'][token] = frequency_dist[token]
+                if token in tf_data_matrix:
+                    tf_data_matrix[token] = frequency_dist[token]
+                self.tf_dict[doc_id]['term_frequencies'] = dict.copy(tf_data_matrix)
 
 
 
@@ -174,7 +193,7 @@ class Preprocessor:
             for topic in self.parsed_data[doc_id]['topics']:
                 self.topic_wise_dict[topic][doc_id] = feature_dict[doc_id]
             #feature_dict[doc_id]['topics'] = self.parsed_data[doc_id]['topics']
-            #feature_dict[doc_id]['places'] = self.parsed_data[doc_id]['places']
+            feature_dict[doc_id]['places'] = self.parsed_data[doc_id]['places']
         #for topic in self.topic_list:
 
         return feature_dict
@@ -194,6 +213,15 @@ class Preprocessor:
             # self.word_dict = OrderedDict(sorted(self.tfidf_dict[doc_id]['tfidf'].items(), key=lambda t: t[1], reverse = True))
 
 
+    def convert_to_utf(self, input):
+        if isinstance(input, dict):
+            return {self.convert_to_utf(key): self.convert_to_utf(value) for key, value in input.iteritems()}
+        elif isinstance(input, list):
+            return [self.convert_to_utf(element) for element in input]
+        elif isinstance(input, unicode):
+            return input.encode('utf-8')
+        else:
+            return input
 
 
 
@@ -210,16 +238,15 @@ def main():
     preprocessor.clean_data()
     end = time.clock()
     print end - start, 'seconds to remove stop words and stem all bodies of documents'
-    PreprocessorHelper.write_to_file(preprocessor.parsed_data, "cleaned.json")
-
+    PreprocessorHelper.write_to_file(preprocessor.tokenized_body_cleaned_dict, "cleaned_small.json")
 
     start = time.clock()
     preprocessor.populate_tfidf_feature_vector_dictionary()
     end = time.clock()
     print end - start, 'seconds to populate tfidf feature vector'
 
-    PreprocessorHelper.write_to_file(preprocessor.topic_wise_dict,"tfidf.json")
-    preprocessor.clear_topic_dict()
+    #PreprocessorHelper.write_to_file(preprocessor.topic_wise_dict,"tfidf.json")
+    #preprocessor.clear_topic_dict()
 
     start = time.clock()
     preprocessor.populate_term_frequencies_feature_vector_dictionary()
@@ -227,9 +254,9 @@ def main():
     print end - start, 'seconds to populate tf feature vector'
 
 
-    PreprocessorHelper.write_to_file(preprocessor.topic_wise_dict,"term_frequencies.json")
+    PreprocessorHelper.write_to_file(preprocessor.topic_wise_dict,"term_frequencies_small.json")
     preprocessor.clear_topic_dict()
-
+'''
     start = time.clock()
     preprocessor.populate_bigram_feature_vector()
     end = time.clock()
@@ -238,5 +265,5 @@ def main():
 
     PreprocessorHelper.write_to_file(preprocessor.topic_wise_dict, "bigrams_pmi.json")
     preprocessor.clear_topic_dict()
-
+    '''
 if __name__ == "__main__": main()
